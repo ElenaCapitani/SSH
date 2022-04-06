c27829daa93ffaa8aa93cb3c49cc28b02eb412b8
Elena <elena.capitani@fermi.mo.it>
2022-04-06 08:58:47 +0200

    My SSH app, first commit

---
+++ blog/models.py
@@ -0,0 +1,19 @@
+from django.conf import settings 
+from django.db import models 
+from django.utils import timezone 
+
+
+class Post(models.Model):
+     id = models.AutoField(primary_key=True)
+     data_ora = models.DateTimeField(default=None)
+     temp = models.IntegerField(default=None)
+     hum = models.IntegerField(default=None)
+     co2 = models.IntegerField(default=None)
+     voc = models.IntegerField(default=None)
+     pmio = models.IntegerField(default=None)
+     pm25 = models.IntegerField(default=None)
+
+
+
+     def __str__(self):
+         return self.data_ora
